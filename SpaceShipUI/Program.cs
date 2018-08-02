7ff38c24a2f5b086330b615b777fcde0d286a0b7
Robert Page <rob@srcapture.com>
2018-08-02 16:43:29 +0100

    initial ui project

---
+++ SpaceShipUI/Program.cs
@@ -0,0 +1,19 @@
+﻿using Spaceships;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceShipUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var game = new GameArea();
+
+            
+        }
+    }
+}
